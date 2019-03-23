71a6e3e8dbe7b7b80c7a3fec70562e481dbf9ccc
Adolfo Amaris <adolfo.amaris@banlinea.com>
2019-03-23 15:33:21 -0500

    ForecastItem

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './styles.css';
+import ForecastItem from './ForecastItem';
 
 class ForecastExtended extends Component{
     render(){
@@ -8,6 +9,7 @@ class ForecastExtended extends Component{
         return (
         <div>
            <h2 className="forecast-title"> Pronóstico Extendido para {city}</h2>
+           <ForecastItem weekDay={'Lunes'}></ForecastItem>
         </div>);
     }
 }
---
+++ src/components/ForecastItem/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ForecastItem = ({ weekDay }) => (
+    <div>{ weekDay }</div>
+);
+
+ForecastItem.propTypes = {
+    weekDay: PropTypes.string.isRequired
+}
+
+
+export default ForecastItem;
