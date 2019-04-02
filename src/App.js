b36c5d0fdee742a8c47d96748efaa86112760739
Adolfo Amaris <amaris91@gmail.com>
2019-04-01 22:32:53 -0500

    Inicio de Redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
-
 import { Grid, Col, Row } from 'react-flexbox-grid';
 import LocationList from "./components/LocationList";
 import ForecastExtended from "./components/ForecastExtended";
+import { setCity } from './actions';
 
 import "./App.css";
 
@@ -18,18 +19,21 @@ const cities = [
   "Madrid,es",
   "Lima,pe"
 ];
+
+
 class App extends Component {
 
   constructor() {
-      super();
-      this.state = { city: null };
-  }    
+    super();
+    this.state = { city: null };
+  }
   handleSelectedLocation = city => {
-      this.setState({city: city});
+    this.setState({ city: city });
     console.log(`handleSelectedLocation ${city}`);
+    this.props.setCity(city);
   };
   render() {
-      const { city } = this.state;
+    const { city } = this.state;
     return (
       <Grid>
         <Row>
@@ -43,20 +47,20 @@ class App extends Component {
         </Row>
         <Row>
           <Col xs={12} md={6}>
-            <LocationList 
-              cities={cities} 
+            <LocationList
+              cities={cities}
               onSelectedLocation={this.handleSelectedLocation}>
             </LocationList>
           </Col>
           <Col xs={12} md={6}>
-          <Paper elevation={4}>
-            <div className="details">
-                { 
-                    city &&
-                        <ForecastExtended city={city}></ForecastExtended>
+            <Paper elevation={4}>
+              <div className="details">
+                {
+                  city &&
+                  <ForecastExtended city={city}></ForecastExtended>
                 }
-            </div>
-          </Paper>
+              </div>
+            </Paper>
           </Col>
         </Row>
       </Grid>
@@ -64,4 +68,11 @@ class App extends Component {
   }
 }
 
-export default App;
+
+const mapDispatchToPropsActions = dispatch => ({
+  setCity: value => dispatch(setCity(value))
+});
+
+const AppConnected = connect(null, mapDispatchToPropsActions)(App);
+
+export default AppConnected;
